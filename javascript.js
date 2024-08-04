bdd9fafc1712b31bfbb8c639620d20fe96c29254
Monika-srivastav <srivastavmonika295@gmail.com>
2024-08-04 09:25:18 +0530

    Add files

---
+++ javascript.js
@@ -0,0 +1,58 @@
+// let search-document.querySelector("search-form")
+// function function(){
+//     slidesearch.style.display="block";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let navbar=document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick= ()=>{
+    navbar.classList.toggle('active');
+    
+    // searchForm.classList.remove('active');
+    // cartItem.classList.remove('active');
+}
+let searchForm= document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick=()=>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    
+    cartItem.classList.remove('active');
+}
+
+let cartItem= document.querySelector('.cart-items-container');
+document.querySelector('#cart-btn').onclick=()=>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
